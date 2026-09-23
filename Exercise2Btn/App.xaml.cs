383b0eeb6a091e1600f5a3afb1a1e454b7eb3203
abdirahman220923 <220923@site.asty-moulin.be>
2026-09-23 11:47:57 +0200

    Add files via upload

---
+++ Exercise2Btn/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Exercise2btn
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
